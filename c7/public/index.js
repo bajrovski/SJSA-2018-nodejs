045076c5003d450854c3d765a872ae7dd102468b
Emil Bajrovski <bajrovski@gmail.com>
2018-03-14 21:57:04 +0100

    Express

---
+++ c7/public/index.js
@@ -0,0 +1,23 @@
+var fname = document.querySelector('.firstname');
+var lname = document.querySelector('.lastname');
+var btnAddUser = document.querySelector('#add-user');
+
+btnAddUser.addEventListener('click', function(){
+    var data = {
+        firstname: fname.value,
+        lastname: lname.value,
+        
+    };
+
+    fetch(
+        'http://127.0.0.1:3000/api/users',
+        {
+            method: 'POST',
+            body: JSON.stringify(data)
+        }
+    ).then(function(res){
+        return res.text();
+    }).then(function(data){
+        console.log(data)
+    });
+});
